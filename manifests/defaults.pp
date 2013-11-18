efeb46a9c737120cb388238c06231a09426d4c89
Stefan van Wouw <stefanvanwouw@gmail.com>
2013-11-18 16:17:35 +0100

    Working Spark 0.8.0

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -1,9 +1,10 @@
 class shark::defaults {
 
     $hive_compatible_version = '0.9.0'
-    $spark_worker_memory = '4g'
-    $hadoop_version = 'cdh4' # Can be either 'cdh4' or 'hadoop1'.
-    $spark_version = '0.8.0'
-    $shark_version = '0.8.0'
-    $scala_version = '2.9.3'
+    $spark_worker_memory     = '4g'
+    $hadoop_version          = 'cdh4' # Can be either 'cdh4' or 'hadoop1'.
+    $spark_version           = '0.8.0'
+    $shark_version           = '0.8.0'
+    $scala_version           = '2.9.3'
+    $java_home               = '/usr/lib/jvm/java-6-oracle'
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class shark (
     $master,
-    $slaves,
     $hive_compatible_version = $::shark::defaults::hive_compatible_version,
     $hadoop_version = $::shark::defaults::hadoop_version,
     $spark_version = $::shark::defaults::spark_version,
     $spark_worker_memory = $::shark::defaults::spark_worker_memory,
     $shark_version = $::shark::defaults::shark_version,
     $scala_version = $::shark::defaults::scala_version,
+    $java_home     = $::shark::defaults::java_home,
 ) inherits shark::defaults {
 
     
@@ -32,31 +32,28 @@ class shark (
         creates => "/opt/shark-${shark_version}-bin-${hadoop_version}",
         require => File['/opt/fetch_requirements.sh'],
     }
-    
-    file {"/opt/spark-${spark_version}/conf/slaves":
-        content => template('shark/slaves.erb'),
-        require => Exec['fetch_requirements'],
-    }
 
-    file {"/opt/spark-${spark_version}/conf/spark-env.sh":
-        content => template('shark/spark-env.sh.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0744',
+    file {'/opt/shark-0.8.0-bin-cdh4':
+        ensure  => directory,
+        recurse => true,
+        owner   => 'spark',
+        group   => 'spark',
         require => Exec['fetch_requirements'],
     }
+    
+
     file {"/opt/shark-${shark_version}-bin-${hadoop_version}/shark-${shark_version}/conf/shark-env.sh":
         content => template('shark/shark-env.sh.erb'),
-        owner   => 'root',
-        group   => 'root',
+        owner   => 'spark',
+        group   => 'spark',
         mode    => '0744',
         require => Exec['fetch_requirements'],
     }
 
     file {"/opt/shark-${shark_version}-bin-${hadoop_version}/hive-${hive_compatible_version}-shark-${shark_version}-bin/conf/hive-site.xml":
         source  => '/etc/hive/conf/hive-site.xml',
-        owner   => 'root',
-        group   => 'root',
+        owner   => 'spark',
+        group   => 'spark',
         mode    => '0644',
         require => Exec['fetch_requirements'],
     }
